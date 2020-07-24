aac7cf8bf71d121f9e2b3dcb5f70a606ad2184ee
Kishore <kishoresreedhar1999@gmail.com>
2020-07-24 15:51:00 +0530

    Updates

---
+++ precache-manifest.ce5495282c69deec72df0e3bbad93520.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2f520e6a80610e7685baac0b713fda",
+    "url": "/iamkish0re.github.io/index.html"
+  },
+  {
+    "revision": "a6a90b38fff6aa7800bb",
+    "url": "/iamkish0re.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e2d5499fdbdbfa94c99f",
+    "url": "/iamkish0re.github.io/static/js/2.c662c429.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iamkish0re.github.io/static/js/2.c662c429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6a90b38fff6aa7800bb",
+    "url": "/iamkish0re.github.io/static/js/main.fcba24a2.chunk.js"
+  },
+  {
+    "revision": "f1b3df3878f5662991c5",
+    "url": "/iamkish0re.github.io/static/js/runtime-main.0c4770e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/iamkish0re.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
